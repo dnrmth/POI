25f7129ff38d6dea4106c7afa8edb20dc294e8b4
Denner Matheus <dennermat@gmail.com>
2024-11-20 18:33:07 -0300

    Created Exception Handler class

---
+++ src/main/java/com/POI/exception/Exception.java
@@ -0,0 +1,15 @@
+package com.POI.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class Exception {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> IllegalArgumentHandler(IllegalArgumentException ex){
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+}
